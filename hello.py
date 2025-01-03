968ca19e86d6275e06df9b390df2ea4806560990
Kathrin <kohlerka@gmx.ch>
2025-01-03 16:51:19 +0100

    Add hello.py file

---
+++ hello.py
@@ -0,0 +1,5 @@
+# This is a hello-word program in Python to also test vscode on the ubuntu computer
+name = input("What's your name? ")
+last_name = input("What's your last name? ")
+print("Hello, " + name + "!")
+print("Hello,", name, last_name,"!")
