685a6ef4fc0752aa1caf84631dca1c8e01bd4883
Kathrin <kohlerka@gmx.ch>
2025-01-04 13:55:42 +0100

    Code from Integer section

---
+++ integer.py
@@ -0,0 +1,11 @@
+x = input("Enter x ")
+y = input("Enter y ")
+z = x + y # This will concatenate the two strings
+print("x + y =", z, "it's concatenated")
+z = int(x) + int(y) # This will add the two integers
+print("x + y =", z, "it's added")
+
+# Nest functions
+a = int(input("Enter a "))
+b = int(input("Enter b "))
+print("a + b =", a + b)
