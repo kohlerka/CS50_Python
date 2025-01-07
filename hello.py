0544268c41e2bf05dc5c9c0eef41a9692e310547
MarkKraeuchi <mark.kraeuchi@bluewin.ch>
2025-01-07 20:17:07 +0100

    Mark's first changes

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 # This is a hello-word program in Python to also test vscode on the ubuntu computer
 name = input("What's your name? ")
 last_name = input("What's your last name? ")
+home_town = input("Where are you from? ")
 print("Hello, " + name + "!")
 print("Hello,", name, last_name,"!")
 print("I need to understand github and git better")
