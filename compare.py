1cf32d4778ddefc9ee49cf6bc37b3e158d2229e7
kohlerka <kohlerka@gmx.ch>
2025-02-19 19:57:38 +0100

    änderung nach Pause

--- compare.py
+++ compare.py
@@ -7,3 +7,21 @@ elif x > y:
     print("x is greater than y")
 else:
     print("x is equal to y")
+
+#There is also another way of doing it
+print("Version with or")
+if x < y or x > y:
+    print("x is not equal to y") 
+else:    
+    print("x is equal to y")       
+
+#And now you can ask if it's not equal to y
+print("version with not equal")
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+# Just to show the "and" operator
+print("Version with and")
+   
